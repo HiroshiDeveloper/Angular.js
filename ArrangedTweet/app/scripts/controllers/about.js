769b4f611a9070a9277f3e3d15ec0edd5b42d12a
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-10-09 13:48:50 -0700

    first Angular.js

---
+++ ArrangedTweet/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arrangedTweetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the arrangedTweetApp
+ */
+angular.module('arrangedTweetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ArrangedTweet/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arrangedTweetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arrangedTweetApp
+ */
+angular.module('arrangedTweetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
